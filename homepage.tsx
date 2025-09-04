0ab8d7c0fb58811a73530b8cc387aa2e60fe8934
Paul Healy <jphealy@udel.edu>
2025-09-04 13:43:16 -0400

    Deleted the tsx file and added in a function

--- homepage.tsx
+++
@@ -1,8 +0,0 @@
-function greet(name: string): string {
-    return `Greetings, ${name}! Welcome to James Paul Healy's website.`;
-}
-
-const messageElement = document.getElementById('message');
-if (messageElement) {
-    messageElement.textContent = greet("User");
-}
